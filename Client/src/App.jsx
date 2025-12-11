985c4ef1b8d1c967ca74be910ef7d88149602b2b
Andersonwillis <Andersonmw1@masters.edu>
2025-12-10 17:54:15 -0800

    fix: remove unnecessary React import statement

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { getFollowers, getOverview, getTotalFollowers, patchOverview } from './api';
 import Header from './components/Header.jsx';
 import FollowerCard from './components/FollowerCard.jsx';
